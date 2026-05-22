77696c4c0e63eff1e8b15db0c077df7a185a9438
ccox0719 <ccox0719@gmail.com>
2026-05-22 10:39:04 -0500

    Inject Supabase config at build time

--- build-config.js
+++ build-config.js
@@ -5,8 +5,16 @@ const config = {
   anonKey: process.env.SUPABASE_ANON_KEY || process.env.VITE_SUPABASE_ANON_KEY || '',
 };
 
-fs.writeFileSync(
-  'env.js',
-  `window.DEVOT_SUPABASE_CONFIG = ${JSON.stringify(config, null, 2)};\n`,
-  'utf8'
+const htmlPath = 'index.html';
+const html = fs.readFileSync(htmlPath, 'utf8');
+const next = html.replace(
+  'window.DEVOT_SUPABASE_CONFIG = {};',
+  `window.DEVOT_SUPABASE_CONFIG = ${JSON.stringify(config, null, 2)};`
 );
+
+if (next === html) {
+  throw new Error('Could not find config placeholder in index.html');
+}
+
+fs.writeFileSync(htmlPath, next, 'utf8');
+fs.writeFileSync('env.js', `window.DEVOT_SUPABASE_CONFIG = ${JSON.stringify(config, null, 2)};\n`, 'utf8');
