d023635915da0af4146c79b5090913586c405cbc
ccox0719 <ccox0719@gmail.com>
2026-05-22 10:50:18 -0500

    Make config build step nonblocking

--- build-config.js
+++ build-config.js
@@ -5,16 +5,8 @@ const config = {
   anonKey: process.env.SUPABASE_ANON_KEY || process.env.VITE_SUPABASE_ANON_KEY || '',
 };
 
-const htmlPath = 'index.html';
-const html = fs.readFileSync(htmlPath, 'utf8');
-const next = html.replace(
-  'window.DEVOT_SUPABASE_CONFIG = {};',
-  `window.DEVOT_SUPABASE_CONFIG = ${JSON.stringify(config, null, 2)};`
+fs.writeFileSync(
+  'env.js',
+  `window.DEVOT_SUPABASE_CONFIG = ${JSON.stringify(config, null, 2)};\n`,
+  'utf8'
 );
-
-if (next === html) {
-  throw new Error('Could not find config placeholder in index.html');
-}
-
-fs.writeFileSync(htmlPath, next, 'utf8');
-fs.writeFileSync('env.js', `window.DEVOT_SUPABASE_CONFIG = ${JSON.stringify(config, null, 2)};\n`, 'utf8');
